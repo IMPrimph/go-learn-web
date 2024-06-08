c0a6a3d85155b4a3678930d5f75ec17fdef6cc4b
Jaswanth <68636066+IMPrimph@users.noreply.github.com>
2024-06-08 18:50:22 +0530

    Add files via upload

---
+++ cmd/web/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+	"web-app/pkg/config"
+	"web-app/pkg/handlers"
+	"web-app/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNum = ":9090"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	log.Println("Server started running on port ", portNum)
+
+	srv := &http.Server{
+		Addr:    portNum,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
